7576860e81c08d812f6a27d5234c4467a0549818
Jefferson <jefry04@gmail.com>
2022-07-17 16:19:50 -0500

    create endpoint to recover all bookin for a user

--- sfr-service/server/controllers/user.controller.js
+++ sfr-service/server/controllers/user.controller.js
@@ -1,18 +1,22 @@
 const User = require('../models/user.model');
-const Field = require ('../models/field.model');
-module.exports = {
+const Field = require('../models/field.model');
+const Booking = require('../models/booking.model');
 
+module.exports = {
   async show(req, res) {
     try {
       //TODO mostrar solo las bookingdate
       const userId = req.user;
-      const user = await User.findById(userId).populate('bookings', 'bookingDate')
+      const user = await User.findById(userId).populate(
+        'bookings',
+        'bookingDate'
+      );
 
       if (!user) {
         throw new Error('User not found');
       }
 
-      res.status(200).json({user})
+      res.status(200).json({ user });
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
@@ -21,9 +25,11 @@ module.exports = {
   async getFieldByUserId(req, res) {
     try {
       const userId = req.user;
-      console.log(userId)
-      const user = await User.findById(userId).populate('bookings', 'bookingDate')
-      console.log(user)
+      const user = await User.findById(userId).populate(
+        'bookings',
+        'bookingDate'
+      );
+      console.log(user);
       if (!user) {
         throw new Error('User not found');
       }
@@ -35,6 +41,29 @@ module.exports = {
       }
 
       res.status(200).json(fields);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
+
+  async getUserBookings(req, res) {
+    try {
+      const userId = req.user;
+      const user = await User.findById(userId).populate(
+        'bookings',
+        'bookingDate'
+      );
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      const bookings = await Booking.find({ userId: user.id });
+      if (!bookings) {
+        res.status(403).json({ message: 'user does not have booking yet' });
+        return;
+      }
+
+      res.status(200).json(bookings);
     } catch (error) {
       res.status(400).json(error);
     }
--- sfr-service/server/routes/user.js
+++ sfr-service/server/routes/user.js
@@ -4,5 +4,6 @@ const { userAuth } = require('../middlewares/userAuth.middleware');
 
 router.route("/profile").get(userAuth, userController.show);
 router.route('/profile/fields').get(userAuth, userController.getFieldByUserId)
+router.route('/profile/bookings').get(userAuth, userController.getUserBookings)
 
 module.exports = router;
